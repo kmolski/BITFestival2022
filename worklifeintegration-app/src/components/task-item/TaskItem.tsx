f5b4ec49189be9a1d66c5c545091e57fc97ebfc3
Gazda70 <pjgazda@interia.pl>
2022-12-03 16:53:10 +0100

    remove task component

--- worklifeintegration-app/src/components/task-item/TaskItem.tsx
+++ worklifeintegration-app/src/components/task-item/TaskItem.tsx
@@ -8,20 +8,24 @@ import Stack from '@mui/material/Stack';
 import Divider from '@mui/material/Divider';
 import Typography from '@mui/material/Typography';
 import { Card, Modal } from '@mui/material';
-import RemoveTask from '../remove-task/RemoveTask';
-import { EventData, State, EventObject, TypegenDisabled } from 'xstate';
+import { sendType, stateType } from '../../machines/types';
 
-{/* <div className="TaskItem">
-<>{props.task.name}</>
-</div> */}
-function setState(){
 
-}
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 interface Dictionary<T> {
   [Key: string]: T;
 }
-
 const dictionary_of_colors: any = {"LOW":"green", "MEDIUM":"cyan", "HIGH":"red"};
 export function TaskItem(props: {task: Task, height:number, }) {
   const [show, setShow] = useState(false);
@@ -39,9 +43,26 @@ export function TaskItem(props: {task: Task, height:number, }) {
         <Typography color="text.secondary" variant="body2">
           Godzina taska
         </Typography>
-        <RemoveTask task={props.task} send={function (event: any, payload?: EventData | undefined): State<any, EventObject, any, { value: any; context: any; }, TypegenDisabled> {
-        throw new Error('Function not implemented.');
-      } }/>
+        <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+            <Box>
+                <tr>Name: {props.task.name}</tr>
+                <tr>Start time: {props.task.start.format('MMMM Do YYYY, h:mm:ss a')}</tr>
+                <tr>End time: {props.task.end.format('MMMM Do YYYY, h:mm:ss a')}</tr>
+                <tr>Place: {props.task.placeId}</tr>
+                <tr>Placement limit: {props.task.placementLimitId}</tr>
+                <tr>Priority: {props.task.taskPriority}</tr>
+            </Box>
+          <Button variant="contained" color="error" 
+          onClick={()=>{props.send('DELETE', {id: props.task.id});
+          setOpen(false)}}>Remove</Button>
+        </Box>
+      </Modal>
     </Card>
   );
 }
